3f84e55e47bc45847b69584c060865d968320548
georgefelipee <georgefelipe938@gmail.com>
2024-04-05 15:55:27 -0300

    start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from "@/components/ConteudoPrincipal.vue";
+
+export default {
+  components: {ConteudoPrincipal, Banner }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
